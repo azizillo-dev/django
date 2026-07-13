92537375ea2a3e33f193cc324a9ba396262a66e6
Azizillo <azizillonabiyev52@gmail.com>
2026-07-13 09:25:50 +0500

    1-vazifa

--- lesson8/vazifa1/movies/admin.py
+++ lesson8/vazifa1/movies/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id' ,"title", "year", 'producer', 'category', 'genre']
+    search_fields = ['description', 'title', 'genre', 'producer', 'category']
+    list_filter = ['producer', 'category']
+
+admin.site.register(Producer)
+admin.site.register(Category)
+
+
+
--- lesson8/vazifa1/movies/models.py
+++ lesson8/vazifa1/movies/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-
+from django.core.validators import MinLengthValidator, MaxLengthValidator, FileExtensionValidator
 
 
 class BaseModel(models.Model):
@@ -10,21 +9,49 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
-
-class Movie(BaseModel):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    image = models.ImageField(upload_to='movies/')
-    year = models.IntegerField()
-    genre = models.CharField(max_length=255)
-    rating = models.FloatField()
-
-
+class Category(BaseModel):
+    title = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.title
 
 
 
+class Producer(BaseModel):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=100)
+    address = models.TextField()
 
+
+    def __str__(self):
+        return self.full_name
+
+
+
+class Movie(BaseModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(
+        upload_to='movies/', 
+        default='default.jpg',
+        validators=[
+            # MinLengthValidator(1), 
+            # MaxLengthValidator(30),
+            FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'gif', 'webp'])
+        ]
+    )
+    year = models.IntegerField()
+    genre = models.CharField(max_length=255)
+    rating = models.FloatField()
+    producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.title} {self.producer.full_name} {self.category.title}"
+
+
+    
 
 
