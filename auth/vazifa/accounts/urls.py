d6a31389377109401f2d7e67678b28149294db67
Azizillo <azizillonabiyev52@gmail.com>
2026-07-19 19:27:22 +0500

    Auth

--- auth/vazifa/accounts/urls.py
+++ auth/vazifa/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, home
 
 
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('', home, name = 'home'),
 ]
 
 
--- auth/vazifa/accounts/views.py
+++ auth/vazifa/accounts/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import CustomUser
 from django.views import View
 
+
+def home(request):
+    return redirect(request, 'home.html')
 
 
 
@@ -10,5 +13,26 @@ class RegisterView(View):
 
     def get(self, request):
         return render(request, 'auth/register.html')
+    
+    def post(self, request):
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        year = request.POST.get('year')
+        address = request.POST.get('address')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        CustomUser.objects.create(
+            first_name = first_name, 
+            last_name = last_name,
+            year = year,
+            username = username,
+            password = password,
+            address = address
+        )
+
+        return redirect('home')
+    
+    
 
 
